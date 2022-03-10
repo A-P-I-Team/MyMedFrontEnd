6658f33243557f075da83e635c2dbe412af39f9c
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-03-11 02:54:07 +0330

    Edit responsive UI of onBoarding page for web rendering

--- my_med/lib/src/modules/intro/pages/onboarding.dart
+++ my_med/lib/src/modules/intro/pages/onboarding.dart
@@ -26,119 +26,123 @@ class _OnboardingPage extends StatelessWidget {
     final provider = context.watch<OnboardingProvider>();
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            const Expanded(
-              flex: 2,
-              child: SizedBox(),
-            ),
-            Expanded(
-              flex: 5,
-              child: PageView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemCount: provider.pageCount,
-                controller: provider.controller,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: MediaQuery.of(context).size.width * .2,
-                    ),
-                    child: Column(
-                      children: [
-                        AspectRatio(
-                          aspectRatio: 1,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).primaryColor,
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            alignment: Alignment.center,
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: Center(
-                            child: Text(
-                              [
-                                "Picture 1",
-                                "Picture 2",
-                                "Picture 3",
-                              ][index],
-                              textAlign: TextAlign.center,
-                              style: Theme.of(context).textTheme.headline6!.merge(
-                                    const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
+        child: SizedBox(
+          width: (MediaQuery.of(context).size.width > 725) ? 725 : MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              const Expanded(
+                flex: 1,
+                child: SizedBox(),
               ),
-            ),
-            Expanded(
-              child: Center(
-                child: SmoothPageIndicator(
-                  effect: JumpingDotEffect(
-                    spacing: 12,
-                    verticalOffset: 16,
-                    activeDotColor: Theme.of(context).buttonTheme.colorScheme!.primary,
-                  ),
-                  count: provider.pageCount,
+              Expanded(
+                flex: 3,
+                child: PageView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: provider.pageCount,
                   controller: provider.controller,
-                  onDotClicked: provider.onDotClicked,
-                ),
-              ),
-            ),
-            const SizedBox(),
-            Expanded(
-              flex: 3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  getButton(
-                    context: context,
-                    route: const LoginRoute(),
-                    buttonType: ButtonType.primary,
-                    child: const Text(
-                      "Sign up & Get your prescriptions fast!",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: (MediaQuery.of(context).size.width > 725) ? 725 * 0.2 : MediaQuery.of(context).size.width * 0.2,
                       ),
-                    ),
-                  ),
-                  getButton(
-                    context: context,
-                    route: const LoginRoute(),
-                    child: const Text.rich(
-                      TextSpan(
-                        style: TextStyle(fontWeight: FontWeight.w700),
+                      child: Column(
                         children: [
-                          TextSpan(
-                            text: 'Have an account?  ',
+                          AspectRatio(
+                            aspectRatio: 1,
+                            child: Container(
+                              height: MediaQuery.of(context).size.height * 0.35,
+                              width: MediaQuery.of(context).size.width * 0.7,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).primaryColor,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              alignment: Alignment.center,
+                            ),
                           ),
-                          TextSpan(
-                            text: 'Login',
-                            style: TextStyle(
-                              fontSize: 16,
+                          Expanded(
+                            flex: 2,
+                            child: Center(
+                              child: Text(
+                                [
+                                  "Picture 1",
+                                  "Picture 2",
+                                  "Picture 3",
+                                ][index],
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.headline6!.merge(
+                                      const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                              ),
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  )
-                ],
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+              Expanded(
+                child: Center(
+                  child: SmoothPageIndicator(
+                    effect: JumpingDotEffect(
+                      spacing: 12,
+                      verticalOffset: 16,
+                      activeDotColor: Theme.of(context).buttonTheme.colorScheme!.primary,
+                    ),
+                    count: provider.pageCount,
+                    controller: provider.controller,
+                    onDotClicked: provider.onDotClicked,
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    getButton(
+                      context: context,
+                      route: const LoginRoute(),
+                      buttonType: ButtonType.primary,
+                      child: const Text(
+                        "Sign up",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    getButton(
+                      context: context,
+                      route: const LoginRoute(),
+                      child: const Text.rich(
+                        TextSpan(
+                          style: TextStyle(fontWeight: FontWeight.w700),
+                          children: [
+                            TextSpan(
+                              text: 'Have an account?  ',
+                            ),
+                            TextSpan(
+                              text: 'Login',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
