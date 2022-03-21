09394a9bb0911da712cc253de09f738771c72a71
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-03-21 21:03:09 +0330

    Add register photo controller

--- my_med/lib/src/modules/intro/models/register_controller.dart
+++ my_med/lib/src/modules/intro/models/register_controller.dart
@@ -1,36 +1,14 @@
 import 'dart:typed_data';
-
 import 'package:flutter/widgets.dart';
 import 'package:my_med/src/models/date.dart';
-import 'package:my_med/src/modules/intro/models/question.dart';
 
 class RegisterController {
   final nameController = TextEditingController();
   Date? birthDate;
   Uint8List? photo;
   String? photoPath;
-  final questions = [
-    Question(
-      question: 'What’s your Gender? *',
-      questionApiValue: 'gender',
-      answerType: AnswerType.radio,
-      answers: [
-        'Male',
-        'Female',
-        'Rather not to say',
-      ],
-      values: [
-        'M',
-        'F',
-        'O',
-      ],
-    ),
-  ];
 
   bool get areQuestionsAnswered {
-    for (final question in questions) {
-      if (question.isRequired && question.selectedAnswer == null) return false;
-    }
     return true;
   }
 }
