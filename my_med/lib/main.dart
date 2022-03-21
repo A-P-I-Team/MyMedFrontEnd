79c26990353851b7fc11dc43ec3fb9df004a56d9
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-03-22 02:58:02 +0430

    Add shared preferences for token and initializing stuff

--- my_med/lib/main.dart
+++ my_med/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:my_med/src/components/utils/shared_preferences.dart';
 import 'package:my_med/src/core/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await PreferencesService.initialize();
   runApp(MyMed());
 }
---
+++ my_med/lib/src/components/utils/shared_preferences.dart
@@ -0,0 +1,26 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferencesService {
+  static late SharedPreferences _storage;
+
+  static Future<void> initialize() async {
+    _storage = await SharedPreferences.getInstance();
+  }
+
+  static String? get getToken {
+    if (_storage.containsKey('Token') == false) {
+      return null;
+    }
+    return _storage.getString('Token');
+  }
+
+  static Future<void> setTokens({
+    required String token,
+  }) async {
+    await _storage.setString('Token', token);
+  }
+
+  static Future<bool> logout() {
+    return _storage.clear();
+  }
+}
