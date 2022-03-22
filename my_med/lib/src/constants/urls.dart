b7848fb5cde1e86169ee962c3ac3f6f1834b3a5d
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-03-22 23:13:42 +0430

    Add new endpoints and constant timeout duration

---
+++ my_med/lib/src/constants/urls.dart
@@ -0,0 +1,6 @@
+class ConstURLs {
+  static const String baseDomain = 'http://mymed.pythonanywhere.com';
+  static const String login = baseDomain + '/user/login/';
+  static const String register = baseDomain + '/user/register/';
+  static const String sendRegisterEmail = baseDomain + '/user/send_register_email/';
+}
