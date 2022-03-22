21dc1966b6e98a3b9adef002aecb6f7cbf1c74a6
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-03-23 02:12:15 +0430

    Add user city model

---
+++ my_med/lib/src/modules/intro/models/city_model.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+List<CityModel> cityModelFromJson(String str) => List<CityModel>.from(json.decode(str).map((x) => CityModel.fromJson(x)));
+
+String cityModelToJson(List<CityModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class CityModel {
+  CityModel({
+    required this.id,
+    required this.cityName,
+  });
+
+  final int id;
+  final String cityName;
+
+  factory CityModel.fromJson(Map<String, dynamic> json) => CityModel(
+        id: json["id"],
+        cityName: json["city_name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "city_name": cityName,
+      };
+}
