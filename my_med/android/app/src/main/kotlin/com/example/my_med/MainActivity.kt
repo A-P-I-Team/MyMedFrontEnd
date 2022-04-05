5ea8483b7bf45ee637e3e6fc56c3b1262ce70481
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-04-05 11:14:44 +0430

    Remove unnecessary changes

--- my_med/android/app/src/main/kotlin/com/example/my_med/MainActivity.kt
+++ my_med/android/app/src/main/kotlin/com/example/my_med/MainActivity.kt
@@ -3,7 +3,4 @@ package com.example.my_med
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine) // add this line
-  }
 }
