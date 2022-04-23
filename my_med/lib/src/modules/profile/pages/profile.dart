fc998e7cd37d8eb9c4a203c8c552d551e12701cf
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-04-24 01:26:54 +0430

    Design profile page UI

--- my_med/lib/src/modules/profile/pages/profile.dart
+++ my_med/lib/src/modules/profile/pages/profile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_med/src/components/custom_app_bar.dart';
+import 'package:my_med/src/modules/profile/components/avatar_column.dart';
+import 'package:my_med/src/modules/profile/components/information_container.dart';
 import 'package:my_med/src/modules/profile/providers/profile_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -19,6 +22,39 @@ class _ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final provider = context.watch<ProfileProvider>();
+    if (provider.hasError) {
+      return const Center(child: Text('Check your internet connection.'));
+    }
+    return Scaffold(
+      appBar: CustomAppbar().buildProfileAppBar(
+        context: context,
+        onNotificationTap: provider.onNotificationTap,
+        onSettingTap: provider.onSettingTap,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            const Expanded(
+              child: AvatarColumn(
+                fullName: 'John Smith',
+                identification: '0032023045',
+              ),
+              flex: 1,
+            ),
+            Expanded(
+              child: InformationContainer(
+                patient: provider.patient!,
+                showEditNamePopUp: provider.showEditNamePopUp,
+                showEditBirthdayPopUp: provider.showEditBirthdayPopUp,
+                showEditSexualityPopUp: provider.showEditSexualityPopUp,
+              ),
+              flex: 3,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
