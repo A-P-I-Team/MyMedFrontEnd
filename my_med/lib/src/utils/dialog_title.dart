982ce4de467247f71e42feed6daa178e963a3f8e
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-04-24 00:50:09 +0430

    Add dialog title and button style component for profile pop ups

---
+++ my_med/lib/src/utils/dialog_title.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DialogTitle {
+  Container buildDialogTitle(
+    String title, {
+    Color backgroundColor = const Color(0xFFF5F7F9),
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: backgroundColor,
+      ),
+      height: 50,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 16.0),
+        child: Text(
+          title,
+          style: const TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 12,
+            color: Color(0xFF474747),
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
