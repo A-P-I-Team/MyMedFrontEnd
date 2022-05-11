eeee99846aa4997755d0bc706bf42d4ca3aec0a3
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-05-11 16:49:15 +0430

    Add doctor and drug models for mocking

---
+++ my_med/lib/src/modules/drug/models/drug_model.dart
@@ -0,0 +1,31 @@
+import 'package:my_med/src/modules/doctor/models/doctor_model.dart';
+
+class DrugModel {
+  final String id;
+  final String name;
+  final double dosage;
+  final DoctorModel doctor;
+  final String consumptionAmount;
+  final String? consumptionStart;
+  final int consumptionTimes;
+  final String consumptionDuration;
+  final String description;
+  final List<dynamic> changeRequests;
+  final bool isActive;
+  final bool isGreen;
+
+  DrugModel({
+    required this.id,
+    required this.name,
+    required this.dosage,
+    required this.doctor,
+    required this.consumptionAmount,
+    required this.consumptionStart,
+    required this.consumptionTimes,
+    required this.consumptionDuration,
+    required this.description,
+    required this.changeRequests,
+    required this.isActive,
+    required this.isGreen,
+  });
+}
