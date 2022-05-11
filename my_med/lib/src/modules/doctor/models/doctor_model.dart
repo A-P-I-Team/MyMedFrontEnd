947e610b88567ae4c9b28d1d82b19ffba39950ef
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-05-11 17:52:05 +0430

    Update doctor mock model

--- my_med/lib/src/modules/doctor/models/doctor_model.dart
+++ my_med/lib/src/modules/doctor/models/doctor_model.dart
@@ -1,11 +1,14 @@
 class DoctorModel {
   final String id;
   final String name;
-  final String profession;
+  final String? profession;
+  final String? imageURL;
 
   DoctorModel({
     required this.id,
     required this.name,
     required this.profession,
+    this.imageURL =
+        "https://www.sketchappsources.com/resources/source-image/doctor-illustration-hamamzai.png",
   });
 }
