4dbc66c6cdee0426a55065a396521ea3f6fe4aa4
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-05-11 17:52:48 +0430

    Develop Doctor page UI + Provider

--- my_med/lib/src/modules/doctor/pages/doctor.dart
+++ my_med/lib/src/modules/doctor/pages/doctor.dart
@@ -1,4 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:my_med/src/components/custom_app_bar.dart';
+import 'package:my_med/src/components/custom_shadow.dart';
+import 'package:my_med/src/l10n/localization_provider.dart';
+import 'package:my_med/src/modules/doctor/components/mini_button.dart';
 import 'package:my_med/src/modules/doctor/providers/doctor_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -8,7 +13,7 @@ class DoctorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => DoctorProvider(),
+      create: (_) => DoctorProvider(context),
       child: const _DoctorPage(),
     );
   }
@@ -19,6 +24,106 @@ class _DoctorPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final provider = context.watch<DoctorProvider>();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: CustomAppbar().buildSearchBar(
+        provider.onSearchTap,
+        provider.isSearchSelect,
+        provider.searchBarController,
+        provider.onSearchChanged,
+        context,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            childAspectRatio: 144 / 184,
+            physics: const AlwaysScrollableScrollPhysics(
+              parent: BouncingScrollPhysics(),
+            ),
+            children:
+                List.generate(provider.doctorListFiltered.length, (index) {
+              var doctor = provider.doctorListFiltered[index];
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: CustomShadow().buildBoxShadow(),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        (doctor.imageURL == null)
+                            ? Image.asset("assets/doctor.png")
+                            : CachedNetworkImage(
+                                imageUrl: doctor.imageURL!,
+                                placeholder: (context, url) => const Center(
+                                    child: CircularProgressIndicator()),
+                                imageBuilder: (context, imageProvider) =>
+                                    Container(
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: imageProvider,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                width: 64,
+                                height: 64,
+                                errorWidget: (context, url, error) =>
+                                    Image.asset("assets/doctor.png"),
+                              ),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                doctor.name,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16,
+                                  color: Color(0xFF474747),
+                                ),
+                                overflow: TextOverflow.clip,
+                                maxLines: 3,
+                                textAlign: TextAlign.center,
+                              ),
+                              const SizedBox(height: 4),
+                              Text(
+                                doctor.profession ??
+                                    context.localizations.unknown,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 10,
+                                  color: Color(0xFF474747),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Flexible(
+                          child: MiniButton(
+                            title: context.localizations.doctorProfileButton,
+                            onTap: () => provider.onDoctorsDetailsTap(
+                                provider.doctorListFiltered[index].id),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }),
+          ),
+        ),
+      ),
+    );
   }
 }
--- my_med/lib/src/modules/doctor/providers/doctor_provider.dart
+++ my_med/lib/src/modules/doctor/providers/doctor_provider.dart
@@ -1,3 +1,82 @@
 import 'package:flutter/cupertino.dart';
+import 'package:my_med/src/modules/doctor/models/doctor_model.dart';
 
-class DoctorProvider extends ChangeNotifier {}
+class DoctorProvider extends ChangeNotifier {
+  BuildContext context;
+  bool isSearchSelect = false;
+  TextEditingController searchBarController = TextEditingController();
+  List<DoctorModel> doctorsList = [];
+  List<DoctorModel> doctorListFiltered = [];
+
+  DoctorProvider(this.context) {
+    getDoctors();
+  }
+
+  void getDoctors() {
+    //TODO API Call (Get Doctors)
+    doctorsList = [
+      DoctorModel(
+        id: '#',
+        name: 'Danial Bazmande',
+        profession: 'ENT Doctor',
+      ),
+      DoctorModel(
+        id: '#',
+        name: 'Mohammad Yarmoghadam',
+        profession: 'Surgery Expert',
+      ),
+      DoctorModel(
+        id: '#',
+        name: 'AmirMahdi Shadman',
+        profession: 'Surgery Expert',
+      ),
+      DoctorModel(
+        id: '#',
+        name: 'Alireza Haghani',
+        profession: 'Physician',
+      ),
+      DoctorModel(
+        id: '#',
+        name: 'Ali Mostofi',
+        profession: 'ENT Doctor',
+      ),
+      DoctorModel(
+        id: '#',
+        name: 'Arash Taherian Khode Asl',
+        profession: 'Hair and Care',
+      ),
+    ];
+    doctorListFiltered.addAll(doctorsList
+        .where((element) => (element.name.contains(searchBarController.text))));
+  }
+
+  void onDoctorsDetailsTap(String id) {
+    //TODO Navigate to doctor details page
+  }
+
+  void onSearchTap() {
+    isSearchSelect = !isSearchSelect;
+    searchBarController.clear();
+    updateList();
+    notifyListeners();
+  }
+
+  void onSearchChanged(String? value) {
+    doctorListFiltered = doctorsList
+        .where((element) => (element.name.contains(searchBarController.text)))
+        .toList();
+    notifyListeners();
+  }
+
+  void updateList() {
+    doctorListFiltered = doctorsList
+        .where((element) => (element.name.contains(searchBarController.text)))
+        .toList();
+  }
+
+  @override
+  void dispose() {
+    searchBarController.dispose();
+    super.dispose();
+  }
+}
