1ce57b708649126f043a810beea7044d02999c4a
Iliya Mirzaei <iliya.mi78@gmail.com>
2022-07-06 22:23:49 +0430

    Add new function for snack bar

--- my_med/lib/src/components/utils/snack_bar.dart
+++ my_med/lib/src/components/utils/snack_bar.dart
@@ -30,4 +30,54 @@ class CustomSnackBar {
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
+
+  SnackBar customShowMessage({
+    required String message,
+    required bool isAndroid,
+    required Color color,
+    EdgeInsets? margin,
+    Icon icon = const Icon(
+      Icons.close_rounded,
+      color: Colors.white,
+    ),
+  }) {
+    return SnackBar(
+      content: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ),
+            ),
+            child: icon,
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Text(
+              message,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+              overflow: TextOverflow.clip,
+              maxLines: 20,
+            ),
+          ),
+        ],
+      ),
+      behavior: SnackBarBehavior.floating,
+      dismissDirection:
+          isAndroid ? DismissDirection.horizontal : DismissDirection.vertical,
+      elevation: 10,
+      backgroundColor: color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      margin: margin,
+    );
+  }
 }
